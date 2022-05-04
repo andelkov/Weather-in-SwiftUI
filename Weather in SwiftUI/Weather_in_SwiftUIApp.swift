5fefabae4f11ae4962497719c7f2d6975bf2f12c
andelkov <andelkov@gmail.com>
2022-05-04 23:50:11 +0200

    Initial Commit

---
+++ Weather in SwiftUI/Weather_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_in_SwiftUIApp.swift
+//  Weather in SwiftUI
+//
+//  Created by Anđelko on 04.May.22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
